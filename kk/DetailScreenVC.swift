cfb72d95ac3b3021c8ae5f196bb3667ac49724ef
Ashok <ashok@thebrewapps.com>
2024-07-25 17:42:38 +0530

    added movie details screen

--- kk/DetailScreenVC.swift
+++ kk/DetailScreenVC.swift
@@ -12,8 +12,18 @@ class DetailScreenVC: UIViewController{
     var movie : MovieModel?
     
     @IBOutlet weak var imagePosterBG: UIImageView!
+    
+    @IBOutlet weak var movieTitle: UILabel!
+    
+    @IBOutlet weak var releaseDate: UILabel!
+    
+    @IBOutlet weak var movieDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePosterBG.sd_setImage(with: URL(string: (movie?.posterPath)!))
+        movieTitle.text = movie?.title
+        releaseDate.text = movie?.releaseDate
+        movieDescription.text = movie?.overview
     }
 }
